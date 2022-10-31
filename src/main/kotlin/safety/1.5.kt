d9aae88cff52201240b6d2cfd5f8346a539c267e
Behzod Halil <behzoddev@gmail.com>
2022-10-31 16:40:17 +0900

    [feat::] Add 1.5

---
+++ src/main/kotlin/safety/1.5.kt
@@ -0,0 +1,29 @@
+package safety
+
+@JvmInline
+private value class Name(val value: String)
+
+
+private interface UserRepository {
+    fun load(): MutableMap<Int, User>
+}
+
+private class DefaultUserRepository: UserRepository {
+    private val storedUsers: MutableMap<Int, User> = mutableMapOf()
+
+    override fun load() = storedUsers
+}
+
+private fun main() {
+    val userRepo = DefaultUserRepository()
+
+    val storedUsers = userRepo.load()
+
+    // This is a dangerous way
+    storedUsers[1] = User(name ="Behzod")
+    println(storedUsers)
+
+    storedUsers[2] = User(name = "Behzod")
+    println(storedUsers)
+
+}
