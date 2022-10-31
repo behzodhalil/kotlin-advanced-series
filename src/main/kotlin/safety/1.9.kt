de20eca4e0d79ad3a4b76c5b51e3fd3fe26cd29c
Behzod Halil <behzoddev@gmail.com>
2022-10-31 16:40:49 +0900

    [feat::] Add 1.9

---
+++ src/main/kotlin/safety/1.9.kt
@@ -0,0 +1,53 @@
+package safety
+
+import kotlin.properties.Delegates
+
+data class Record(
+    val name: String,
+    val size: Int
+)
+
+interface LocalSourceManager {
+    fun saveRecord(record: Record)
+}
+
+class DefaultLocalSourceManager: LocalSourceManager {
+    override fun saveRecord(record: Record) {
+        println("Name: ${record.name};\nSize: ${record.size};")
+    }
+}
+
+interface Solution {
+    fun build()
+}
+
+class DefaultSolution: Solution {
+    private lateinit var manager: LocalSourceManager
+
+    override fun build() {
+        manager = DefaultLocalSourceManager()
+        manager.saveRecord(Record(name = "Record #1", size = 10))
+    }
+}
+
+class DefaultSolution2: Solution {
+    /**
+     * One reason why they are so popular is that they help us safely avoid nullability
+     */
+    private var manager: LocalSourceManager by Delegates.notNull()
+
+    override fun build() {
+        manager = DefaultLocalSourceManager()
+        manager.saveRecord(Record(name = "Record #2", size = 12))
+    }
+}
+
+
+
+fun main() {
+    val solution = DefaultSolution()
+    solution.build()
+
+    val solution2 = DefaultSolution2()
+    solution2.build()
+}
